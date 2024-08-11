0ee13dd41a5e461e55687363364a492611684f94
codebender <khoa28122005@gmail.com>
2024-08-11 14:34:32 +0700

    Add Options Binding UI

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -6,7 +6,19 @@ public class GameInput : Singleton<GameInput>
     public event EventHandler OnInteractAction;
     public event EventHandler OnInteractAlternateAction;
     public event EventHandler OnPauseAction;
+
+    public enum Binding
+    {
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        Interact,
+        InteractAlternate,
+        Pause,
+    }
     private PlayerInputActions playerInputActions; 
+
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
@@ -42,5 +54,27 @@ public class GameInput : Singleton<GameInput>
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         inputVector = inputVector.normalized;
         return inputVector;
+    }
+
+    public string GetBindingText(Binding binding)
+    {
+        switch (binding)
+        {
+            default:
+            case Binding.MoveUp:
+                return playerInputActions.Player.Move.bindings[1].ToDisplayString();
+            case Binding.MoveDown:
+                return playerInputActions.Player.Move.bindings[2].ToDisplayString();
+            case Binding.MoveLeft:
+                return playerInputActions.Player.Move.bindings[3].ToDisplayString();
+            case Binding.MoveRight:
+                return playerInputActions.Player.Move.bindings[4].ToDisplayString();
+            case Binding.Interact:
+                return playerInputActions.Player.Interact.bindings[0].ToDisplayString();
+            case Binding.InteractAlternate:
+                return playerInputActions.Player.InteractAlternate.bindings[0].ToDisplayString();
+            case Binding.Pause:
+                return playerInputActions.Player.Pause.bindings[0].ToDisplayString();
+        }
     }
 }
--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -10,8 +10,22 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private Button soundEffectsButton;
     [SerializeField] private Button musicButton;
     [SerializeField] private Button closeButton;
+    [SerializeField] private Button moveUpButton;
+    [SerializeField] private Button moveDownButton;
+    [SerializeField] private Button moveLeftButton;
+    [SerializeField] private Button moveRightButton;
+    [SerializeField] private Button interactButton;
+    [SerializeField] private Button interactAlternateButton;
+    [SerializeField] private Button pause Button;
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
+    [SerializeField] private TextMeshProUGUI moveUpText;
+    [SerializeField] private TextMeshProUGUI moveDownText;
+    [SerializeField] private TextMeshProUGUI moveLeftText;
+    [SerializeField] private TextMeshProUGUI moveRightText;
+    [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private TextMeshProUGUI interactAlternateText;
+    [SerializeField] private TextMeshProUGUI pauseText;
     private void Awake()
     {
         Instance = this;
@@ -46,6 +60,8 @@ public class OptionsUI : MonoBehaviour
     {
         soundEffectsText.text = "Sound Effects: " + Mathf.Round(SoundManager.Instance.GetVolume() * 10f);
         musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10f);
+
+        //moveUpText.text = GameInput.Instance.GetBindingText()
     }
     public void Show()
     {
